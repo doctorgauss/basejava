280f03383962df9333729010777b33235c6290d2
doctorgauss <evigol@mail.ru>
2017-10-13 07:46:15 +0700

    add AbstractStorage MapStorage ListStorage

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -7,49 +7,45 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
 
-public abstract class AbstractArrayStorage extends AbstractStorage {
-    protected static final int STORAGE_LIMIT = 10000;
+import static com.urise.webapp.storage.AbstractArrayStorage.STORAGE_LIMIT;
 
-    protected final Resume[] storage = new Resume[STORAGE_LIMIT];
-    protected int size = 0;
+public abstract class AbstractStorage implements Storage {
 
-    public void clear() {
-        Arrays.fill(storage,0,size,null);
-        size = 0;
-    }
+    protected abstract void doUpdate(Resume r, Object searchKey);
 
-    public void update(Resume r){
+    protected abstract boolean isExistSearchKey();
+
+    protected abstract Object getSearchKey(String uuid);
+
+    protected abstract void doSave();
+
+    public void update(Resume r) {
         if (!check(r)) return;
-        int index = getIndex(r.getUuid());
-        if (index < 0){
+        Object searchKey = getSearchKey(r.getUuid());
+        if (!isExistSearchKey()) {
             throw new NotExistStorageException(r.getUuid());
         } else {
-            storage[index] = r;
+            doUpdate(r, searchKey);
         }
     }
 
     public void save(Resume r) {
         if (!check(r)) return;
-        int index = getIndex(r.getUuid());
-        if (index >= 0){
+        Object searchKey = getSearchKey(r.getUuid());
+        if (isExistSearchKey()) {
             throw new ExistStorageException(r.getUuid());
-        } else if (size >= STORAGE_LIMIT){
-            throw new StorageException("База резюме переполнена", r.getUuid());
-        } else {
-            insertElement(r, index);
-            size++;
-        }
+        } else doSave();
     }
 
     protected abstract void insertElement(Resume r, int index);
 
     public void delete(String uuid) {
         int index = getIndex(uuid);
-        if (index < 0){
+        if (index < 0) {
             throw new NotExistStorageException(uuid);
         } else {
             fillDeletedElement(index);
-            storage[size-1] = null;
+            storage[size - 1] = null;
             size--;
         }
     }
@@ -60,13 +56,13 @@ public abstract class AbstractArrayStorage extends AbstractStorage {
         return Arrays.copyOf(storage, size);
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public Resume get(String uuid){
+    public Resume get(String uuid) {
         int index = getIndex(uuid);
-        if (index < 0){
+        if (index < 0) {
             throw new NotExistStorageException(uuid);
         }
         return storage[index];
@@ -74,10 +70,11 @@ public abstract class AbstractArrayStorage extends AbstractStorage {
 
     protected abstract int getIndex(String uuid);
 
-    protected boolean check(Resume r){
-        if (r == null || r.getUuid() == null){
+    protected boolean check(Resume r) {
+        if (r == null || r.getUuid() == null) {
             throw new StorageException("Резюме не соответствует формату", null);
         }
         return true;
     }
+
 }
